05e87a70f74c8cc0d436bdb39e031ae650a7d03c
wesleybertipaglia <wesleybertipaglia@gmail.com>
2025-07-20 13:32:29 -0300

    chore: init

---
+++ src/main/java/com/wesleybertipaglia/gutendex/GutendexApplication.java
@@ -0,0 +1,13 @@
+package com.wesleybertipaglia.gutendex;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GutendexApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GutendexApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/wesleybertipaglia/gutendex/GutendexApplicationTests.java
@@ -0,0 +1,13 @@
+package com.wesleybertipaglia.gutendex;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GutendexApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
